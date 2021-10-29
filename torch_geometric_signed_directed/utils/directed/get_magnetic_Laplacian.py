fad92d4b7e904a7886e2ddf1f093b83b0d2ebd13
SherylHYX <He_YX@outlook.com>
2021-10-29 04:49:35 +0100

    magnetic laplacian docs

--- torch_geometric_signed_directed/utils/directed/get_magnetic_Laplacian.py
+++ torch_geometric_signed_directed/utils/directed/get_magnetic_Laplacian.py
@@ -35,6 +35,13 @@ def get_magnetic_Laplacian(edge_index: torch.LongTensor, edge_weight: Optional[t
             :obj:`max_val + 1` of :attr:`edge_index`. (default: :obj:`None`)
         q (float, optional): The value q in the paper for phase.
         return_lambda_max (bool, optional): Whether to return the maximum eigenvalue. (default: :obj:`False`)
+
+    Return types:
+        edge_index (PyTorch LongTensor): The edge indices of the magnetic Laplacian.
+        edge_weight.real, edge_weight.imag (PyTorch Tensor): Real and imaginary parts of the
+            one-dimensional edge weights for the magnetic Laplacian.
+        lambda_max (float, optional): The maximum eigenvalue of the magnetic Laplacian, 
+            only returns this when required by setting return_lambda_max as True.
     """
 
     if normalization is not None:
