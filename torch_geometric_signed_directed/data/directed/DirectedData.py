b40c87ecc8936f3058b1653f290364e89da15cde
SherylHYX <He_YX@outlook.com>
2021-11-08 02:29:27 +0000

    directed data class

--- torch_geometric_signed_directed/data/directed/DirectedData.py
+++ torch_geometric_signed_directed/data/directed/DirectedData.py
@@ -1,15 +1,14 @@
-from torch_geometric.typing import OptTensor
+from typing import Any
 
+from torch_geometric.typing import OptTensor
 from torch_geometric.utils import to_scipy_sparse_matrix
 from torch_geometric.data import Data
 from scipy.sparse import spmatrix
 from torch import FloatTensor, LongTensor
 from numpy import array
 
-def DirectedData(x: OptTensor = None, edge_index: OptTensor = None,
-                edge_attr: OptTensor = None, edge_weight: OptTensor = None, y: OptTensor = None,
-                pos: OptTensor = None, A: spmatrix = None, **kwargs):
-    r"""A function to generate a data object describing a homogeneous directed graph.
+class DirectedData(Data):
+    r"""A data object describing a homogeneous directed graph.
 
     Args:
         x (Tensor, optional): Node feature matrix with shape :obj:`[num_nodes,
@@ -27,10 +26,27 @@ def DirectedData(x: OptTensor = None, edge_index: OptTensor = None,
         A (spmatrix, optional): SciPy sparse adjacency matrix. (default: :obj:`None`)
         **kwargs (optional): Additional attributes.
     """
-    if A is None:
-        A = to_scipy_sparse_matrix(edge_index, edge_attr)
-    else:
-        edge_weight = FloatTensor(A.data)
-        edge_index = LongTensor(array(A.nonzero()))
-    return Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y, pos=pos, A=A, \
-        edge_weight=edge_weight, **kwargs)
+    def __init__(self, x: OptTensor = None, edge_index: OptTensor = None,
+                edge_attr: OptTensor = None, edge_weight: OptTensor = None, y: OptTensor = None,
+                pos: OptTensor = None, A: spmatrix = None, **kwargs):
+        super().__init__(x=x, edge_index=edge_index,
+                 edge_attr=edge_attr, y=y,
+                 pos=pos, **kwargs)
+        if A is None:
+            A = to_scipy_sparse_matrix(edge_index, edge_attr)
+        else:
+            edge_weight = FloatTensor(A.data)
+            edge_index = LongTensor(array(A.nonzero()))
+        self.A = A
+        self.edge_weight = edge_weight
+        self.edge_index = edge_index
+        
+
+    @property
+    def edge_weight(self) -> Any:
+        return self['edge_weight'] if 'edge_weight' in self._store else None
+
+    @property
+    def A(self) -> spmatrix:
+        return self['A'] if 'A' in self._store else None
+
