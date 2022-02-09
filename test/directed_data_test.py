d6ff0b4b2f155091ed9d5304a6fe5c7e742b1df6
SherylHYX <He_YX@outlook.com>
2022-02-09 03:25:09 +0000

    is_directed property

--- test/directed_data_test.py
+++ test/directed_data_test.py
@@ -1,5 +1,3 @@
-from torch_geometric.utils import is_undirected
-
 from torch_geometric_signed_directed.data import load_directed_real_data, DirectedData
 
 def test_directed_datasets():
@@ -8,11 +6,11 @@ def test_directed_datasets():
 	"""
 	directed_dataset = load_directed_real_data(dataset='WebKB', root='./tmp_data/', name='Texas')
 	assert isinstance(directed_dataset, DirectedData)
-	assert not is_undirected(directed_dataset.edge_index)
+	assert directed_dataset.is_directed
 	directed_dataset = load_directed_real_data(dataset='WebKB', root='./tmp_data/', name='Cornell')
 	assert isinstance(directed_dataset, DirectedData)
-	assert not is_undirected(directed_dataset.edge_index)
+	assert directed_dataset.is_directed
 	directed_dataset = load_directed_real_data(dataset='WebKB', root='./tmp_data/', name='Wisconsin')
 	assert isinstance(directed_dataset, DirectedData)
-	assert not is_undirected(directed_dataset.edge_index)
+	assert directed_dataset.is_directed
 	return
