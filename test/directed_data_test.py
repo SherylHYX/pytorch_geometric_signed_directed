61ba6b85341dc14f28d7575b7046345cce151ea9
SherylHYX <He_YX@outlook.com>
2022-02-09 03:13:10 +0000

    signed snap data test, and directed refactoring

--- test/directed_data_test.py
+++ test/directed_data_test.py
@@ -1,11 +1,10 @@
-import numpy as np
 from torch_geometric.utils import is_undirected
 
 from torch_geometric_signed_directed.data import load_directed_real_data, DirectedData
 
-def test_datasets():
+def test_directed_datasets():
 	"""
-	Testing load_dataset()
+	Testing load_directed_real_data()
 	"""
 	directed_dataset = load_directed_real_data(dataset='WebKB', root='./tmp_data/', name='Texas')
 	assert isinstance(directed_dataset, DirectedData)
