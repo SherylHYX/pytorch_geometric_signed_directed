ea267bc8fa2d3d88b2c23cf0908e2767f45ddea2
SherylHYX <He_YX@outlook.com>
2022-02-11 15:56:49 +0000

    folder structure

---
+++ torch_geometric_signed_directed/data/directed/WikiCS.py
@@ -0,0 +1,73 @@
+from typing import Optional, Callable
+
+import json
+from itertools import chain
+
+import torch
+from torch_geometric.data import Data, InMemoryDataset, download_url
+
+
+class WikiCS(InMemoryDataset):
+    r"""This is the copy of the torch_geometric.datasets.WikiCS (v1.6.3)
+    Args:
+        root (string): Root directory where the dataset should be saved.
+        transform (callable, optional): A function/transform that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a transformed
+            version. The data object will be transformed before every access.
+            (default: :obj:`None`)
+        pre_transform (callable, optional): A function/transform that takes in
+            an :obj:`torch_geometric.data.Data` object and returns a
+            transformed version. The data object will be transformed before
+            being saved to disk. (default: :obj:`None`)
+    """
+    url = 'https://github.com/pmernyei/wiki-cs-dataset/raw/master/dataset'
+
+    def __init__(self, root: str, transform: Optional[Callable]=None, pre_transform: Optional[Callable]=None):
+        super(WikiCS, self).__init__(root, transform, pre_transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        return ['data.json']
+
+    @property
+    def processed_file_names(self):
+        return 'data.pt'
+
+    def download(self):
+        for name in self.raw_file_names:
+            download_url('{}/{}'.format(self.url, name), self.raw_dir)
+
+    def process(self):
+        with open(self.raw_paths[0], 'r') as f:
+            data = json.load(f)
+
+        x = torch.tensor(data['features'], dtype=torch.float)
+        y = torch.tensor(data['labels'], dtype=torch.long)
+
+        edges = [[(i, j) for j in js] for i, js in enumerate(data['links'])]
+        edges = list(chain(*edges))
+        edge_index = torch.tensor(edges, dtype=torch.long).t().contiguous()
+
+        train_mask = torch.tensor(data['train_masks'], dtype=torch.bool)
+        train_mask = train_mask.t().contiguous()
+
+        val_mask = torch.tensor(data['val_masks'], dtype=torch.bool)
+        val_mask = val_mask.t().contiguous()
+
+        test_mask = torch.tensor(data['test_mask'], dtype=torch.bool)
+
+        stopping_mask = torch.tensor(data['stopping_masks'], dtype=torch.bool)
+        stopping_mask = stopping_mask.t().contiguous()
+
+        data = Data(x=x, y=y, edge_index=edge_index, train_mask=train_mask,
+                    val_mask=val_mask, test_mask=test_mask,
+                    stopping_mask=stopping_mask)
+
+        if self.pre_transform is not None:
+            data = self.pre_transform(data)
+
+        torch.save(self.collate([data]), self.processed_paths[0])
+
+    def __repr__(self):
+        return '{}()'.format(self.__class__.__name__)
