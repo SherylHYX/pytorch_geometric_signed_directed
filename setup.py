943b0c08678267e4ccd0d72c6ad2771aa1836904
Yixuan He <43345664+SherylHYX@users.noreply.github.com>
2022-02-15 17:46:47 +0800

    0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 url = "https://github.com/SherylHYX/pytorch_geometric_signed_directed"
-__version__ = '0.1.0' 
+__version__ = '0.1.1' 
 
 install_requires = [
     "torch",
