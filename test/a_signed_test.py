d712758066b1fb773a19fb4369005cd82bb401fb
SherylHYX <He_YX@outlook.com>
2022-02-15 07:40:20 +0000

    SNEA SGCN docstring and tests

--- test/a_signed_test.py
+++ test/a_signed_test.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.sparse as sp
 import torch
 from torch_geometric_signed_directed.nn.signed import (
-    SSSNET_node_clustering, SDGNN, SNEA, SiGAT, SGCN
+    SSSNET_node_clustering, SDGNN, SGCN_SNEA, SiGAT
 )
 from torch_geometric_signed_directed.data import (
     SSBM, SignedData
@@ -23,6 +23,63 @@ def create_mock_data(num_nodes, num_features, num_classes=3, eta=0.1, p=0.2):
     edge_index_n = torch.LongTensor(np.array(A_n_scipy.nonzero())).to(device)
     edge_weight_n = torch.FloatTensor(sp.csr_matrix(A_n_scipy).data).to(device)
     return X, A_p_scipy, A_n_scipy, edge_index_p, edge_index_n, edge_weight_p, edge_weight_n
+
+def test_SGCN_SNEA():
+    """
+    Testing SGCN and SNEA
+    """
+    num_nodes = 100
+    num_features = 3
+    num_classes = 3
+
+    _, A_p_scipy, A_n_scipy, _, _, _, _ = \
+        create_mock_data(num_nodes, num_features, num_classes)
+
+    data = SignedData(A=(A_p_scipy, A_n_scipy))
+    train_edge_index = data.edge_index
+    train_edge_weight = data.edge_weight
+    test_edge_index = data.edge_index
+    test_edge_weight = data.edge_weight
+    nodes_num = data.num_nodes
+    edge_i_list = train_edge_index.t().cpu().numpy().tolist()
+    edge_s_list = train_edge_weight.long().cpu().numpy().tolist()
+    edge_index_s = torch.LongTensor([[i, j, s] for (i, j), s in zip(edge_i_list, edge_s_list)]).to(device)
+
+    model = SGCN_SNEA('SGCN', nodes_num, edge_index_s, 20, 20, layer_num=2, lamb=5).to(device)
+    loss = model.loss()
+    with torch.no_grad():
+        z = model()
+
+    embeddings = z.cpu().numpy()
+    train_X = train_edge_index.t().cpu().numpy()
+    test_X  = test_edge_index.t().cpu().numpy()
+    train_y = train_edge_weight.cpu().numpy()
+    test_y  = test_edge_weight.cpu().numpy()
+    accuracy, f1, f1_macro, f1_micro, auc_score = link_sign_prediction_logistic_function(embeddings, train_X, train_y, test_X, test_y)
+    assert auc_score >= 0
+    assert loss >= 0
+    assert accuracy >= 0
+    assert f1 >= 0
+    assert f1_macro >= 0
+    assert f1_micro >= 0
+
+    model = SGCN_SNEA('SNEA', nodes_num, edge_index_s, 20, 20, layer_num=2, lamb=5).to(device)
+    loss = model.loss()
+    with torch.no_grad():
+        z = model()
+
+    embeddings = z.cpu().numpy()
+    train_X = train_edge_index.t().cpu().numpy()
+    test_X  = test_edge_index.t().cpu().numpy()
+    train_y = train_edge_weight.cpu().numpy()
+    test_y  = test_edge_weight.cpu().numpy()
+    accuracy, f1, f1_macro, f1_micro, auc_score = link_sign_prediction_logistic_function(embeddings, train_X, train_y, test_X, test_y)
+    assert auc_score >= 0
+    assert loss >= 0
+    assert accuracy >= 0
+    assert f1 >= 0
+    assert f1_macro >= 0
+    assert f1_micro >= 0
 
 def test_SiGAT():
     """
---
+++ torch_geometric_signed_directed/nn/signed/SGCNConv.py
@@ -0,0 +1,139 @@
+from typing import Union
+from torch_geometric.typing import PairTensor, Adj
+import scipy.sparse as sp
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torch import Tensor
+from torch_geometric.nn.dense.linear import Linear
+from torch_sparse import SparseTensor, matmul
+from torch_geometric.nn.conv import MessagePassing
+from torch_sparse import coalesce
+
+from torch_geometric.utils import (negative_sampling,
+                                   structured_negative_sampling)
+
+
+class SGCNConv(MessagePassing):
+    r"""The signed graph convolutional operator from the `"Signed Graph
+    Convolutional Network" <https://arxiv.org/abs/1808.06354>`_ paper
+
+    .. math::
+        \mathbf{x}_v^{(\textrm{pos})} &= \mathbf{\Theta}^{(\textrm{pos})}
+        \left[ \frac{1}{|\mathcal{N}^{+}(v)|} \sum_{w \in \mathcal{N}^{+}(v)}
+        \mathbf{x}_w , \mathbf{x}_v \right]
+
+        \mathbf{x}_v^{(\textrm{neg})} &= \mathbf{\Theta}^{(\textrm{neg})}
+        \left[ \frac{1}{|\mathcal{N}^{-}(v)|} \sum_{w \in \mathcal{N}^{-}(v)}
+        \mathbf{x}_w , \mathbf{x}_v \right]
+
+    if :obj:`first_aggr` is set to :obj:`True`, and
+
+    .. math::
+        \mathbf{x}_v^{(\textrm{pos})} &= \mathbf{\Theta}^{(\textrm{pos})}
+        \left[ \frac{1}{|\mathcal{N}^{+}(v)|} \sum_{w \in \mathcal{N}^{+}(v)}
+        \mathbf{x}_w^{(\textrm{pos})}, \frac{1}{|\mathcal{N}^{-}(v)|}
+        \sum_{w \in \mathcal{N}^{-}(v)} \mathbf{x}_w^{(\textrm{neg})},
+        \mathbf{x}_v^{(\textrm{pos})} \right]
+
+        \mathbf{x}_v^{(\textrm{neg})} &= \mathbf{\Theta}^{(\textrm{pos})}
+        \left[ \frac{1}{|\mathcal{N}^{+}(v)|} \sum_{w \in \mathcal{N}^{+}(v)}
+        \mathbf{x}_w^{(\textrm{neg})}, \frac{1}{|\mathcal{N}^{-}(v)|}
+        \sum_{w \in \mathcal{N}^{-}(v)} \mathbf{x}_w^{(\textrm{pos})},
+        \mathbf{x}_v^{(\textrm{neg})} \right]
+
+    otherwise.
+    In case :obj:`first_aggr` is :obj:`False`, the layer expects :obj:`x` to be
+    a tensor where :obj:`x[:, :in_dim]` denotes the positive node features
+    :math:`\mathbf{X}^{(\textrm{pos})}` and :obj:`x[:, in_dim:]` denotes
+    the negative node features :math:`\mathbf{X}^{(\textrm{neg})}`.
+
+    Args:
+        in_dim (int or tuple): Size of each input sample, or :obj:`-1` to
+            derive the size from the first input(s) to the forward method.
+            A tuple corresponds to the sizes of source and target
+            dimensionalities.
+        out_dim (int): Size of each output sample.
+        first_aggr (bool): Denotes which aggregation formula to use.
+        bias (bool, optional): If set to :obj:`False`, the layer will not learn
+            an additive bias. (default: :obj:`True`)
+        **kwargs (optional): Additional arguments of
+            :class:`torch_geometric.nn.conv.MessagePassing`.
+            
+    """
+
+    def __init__(
+        self,
+        in_dim: int,
+        out_dim: int,
+        first_aggr: bool,
+        bias: bool = True,
+        **kwargs
+    ):
+
+        kwargs.setdefault('aggr', 'mean')
+        super().__init__(**kwargs)
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.first_aggr = first_aggr
+
+        if first_aggr:
+            self.lin_b = Linear(2 * in_dim, out_dim, bias)
+            self.lin_u = Linear(2 * in_dim, out_dim, bias)
+        else:
+            self.lin_b = Linear(3 * in_dim, out_dim, bias)
+            self.lin_u = Linear(3 * in_dim, out_dim, bias)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.lin_b.reset_parameters()
+        self.lin_u.reset_parameters()
+
+    def forward(self, x: Union[Tensor, PairTensor], pos_edge_index: Adj,
+                neg_edge_index: Adj) -> Tensor:
+
+        # propagate_type    e: (x: PairTensor)
+        if isinstance(x, Tensor):
+            x: PairTensor = (x, x)
+
+        if self.first_aggr:
+
+            out_b = self.propagate(pos_edge_index, x=x)
+            out_b = self.lin_b(torch.cat([out_b, x[0]], dim=-1))
+
+            out_u = self.propagate(neg_edge_index, x=x)
+            out_u = self.lin_u(torch.cat([out_u, x[0]], dim=-1))
+
+            return torch.cat([out_b, out_u], dim=-1)
+
+        else:
+            F_in = self.in_dim
+            out_b1 = self.propagate(pos_edge_index, x=(
+                x[0][..., :F_in], x[1][..., :F_in]))
+            out_b2 = self.propagate(neg_edge_index, x=(
+                x[0][..., F_in:], x[1][..., F_in:]))
+            out_b = torch.cat([out_b1, out_b2, x[0][..., :F_in]], dim=-1)
+            out_b = self.lin_b(out_b)
+
+            out_u1 = self.propagate(pos_edge_index, x=(
+                x[0][..., F_in:], x[1][..., F_in:]))
+            out_u2 = self.propagate(neg_edge_index, x=(
+                x[0][..., :F_in], x[1][..., :F_in]))
+            out_u = torch.cat([out_u1, out_u2, x[0][..., F_in:]], dim=-1)
+            out_u = self.lin_u(out_u)
+
+            return torch.cat([out_b, out_u], dim=-1)
+
+    def message(self, x_j: Tensor) -> Tensor:
+        return x_j
+
+    def message_and_aggregate(self, adj_t: SparseTensor,
+                              x: PairTensor) -> Tensor:
+        adj_t = adj_t.set_value(None, layout=None)
+        return matmul(adj_t, x[0], reduce=self.aggr)
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}({self.in_dim}, '
+                f'{self.out_dim}, first_aggr={self.first_aggr})')
