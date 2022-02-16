012f9bb01ae2d8ce36b16c548367e3a2777152ad
SherylHYX <He_YX@outlook.com>
2022-02-16 13:21:25 +0000

    fix seed mask for SSSNET example

--- examples/sssnet.py
+++ examples/sssnet.py
@@ -30,7 +30,7 @@ A = A_p_scipy - A_n_scipy
 A, labels = extract_network(A=A, labels=labels)
 data = SignedData(A=A, y=torch.LongTensor(labels))
 data.set_signed_Laplacian_features(num_classes)
-data.node_split(train_size_per_class=0.8, val_size_per_class=0.1, test_size_per_class=0.1)
+data.node_split(train_size_per_class=0.8, val_size_per_class=0.1, test_size_per_class=0.1, seed_size_per_class=0.1)
 data.separate_positive_negative()
 loss_func_ce = torch.nn.NLLLoss()
 
@@ -40,13 +40,13 @@ optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.w
 data = data.to(device)
 
 def train(features, edge_index_p, edge_weight_p,
-                edge_index_n, edge_weight_n, mask, loss_func_pbnc, y):
+                edge_index_n, edge_weight_n, mask, seed_mask, loss_func_pbnc, y):
     model.train()
     Z, log_prob, _, prob = model(edge_index_p, edge_weight_p,
                 edge_index_n, edge_weight_n, features)
     loss_pbnc = loss_func_pbnc(prob[mask])
-    loss_triplet = triplet_loss_node_classification(y=y[mask], Z=Z[mask], n_sample=500, thre=0.1)
-    loss_ce = loss_func_ce(log_prob[mask], y[mask])
+    loss_triplet = triplet_loss_node_classification(y=y[seed_mask], Z=Z[seed_mask], n_sample=500, thre=0.1)
+    loss_ce = loss_func_ce(log_prob[seed_mask], y[seed_mask])
     loss = args.supervised_loss_ratio*(loss_ce +
                                     args.triplet_loss_ratio*loss_triplet) + loss_pbnc
     optimizer.zero_grad()
@@ -72,11 +72,12 @@ for split in range(data.train_mask.shape[1]):
     train_index = data.train_mask[:, split].cpu().numpy()
     val_index = data.val_mask[:, split]
     test_index = data.test_mask[:, split]
+    seed_index = data.seed_mask[:, split]
     loss_func_pbnc = Prob_Balanced_Normalized_Loss(A_p=sp.csr_matrix(data.A_p)[train_index][:, train_index], 
     A_n=sp.csr_matrix(data.A_n)[train_index][:, train_index])
     for epoch in range(args.epochs):
         train_loss, train_ari = train(data.x, data.edge_index_p, data.edge_weight_p,
-                                        data.edge_index_n, data.edge_weight_n, train_index, loss_func_pbnc, data.y)
+                                        data.edge_index_n, data.edge_weight_n, train_index, seed_index, loss_func_pbnc, data.y)
         Val_ari = test(data.x, data.edge_index_p, data.edge_weight_p,
                         data.edge_index_n, data.edge_weight_n, val_index, data.y)
         print(f'Split: {split:02d}, Epoch: {epoch:03d}, Train_Loss: {train_loss:.4f}, Train_ARI: {train_ari:.4f}, Val_ARI: {Val_ari:.4f}')
