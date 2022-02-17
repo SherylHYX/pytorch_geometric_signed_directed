835c6a432f306a11681f330468d5c2579c3c6920
huangjunjie-cs <junjiehuang.cs@gmail.com>
2022-02-17 18:43:47 +0800

    docstring change

--- torch_geometric_signed_directed/data/signed/SignedDirectedGraphDataset.py
+++ torch_geometric_signed_directed/data/signed/SignedDirectedGraphDataset.py
@@ -19,7 +19,7 @@ dataset_name_url_dic = {
 
 class SignedDirectedGraphDataset(InMemoryDataset):
     r"""Signed Directed Graph from the `"SDGNN: Learning Node Representation 
-    for Signed Directed Networks" <https://arxiv.org/abs/2101.02390>`_ paper, consising of three different
+    for Signed Directed Networks" <https://arxiv.org/abs/2101.02390>`_ paper, consising of four different
     datasets: Bitcoin-Alpha, Bitcoin-OTC， Slashdot and Epinions.
     """
 
