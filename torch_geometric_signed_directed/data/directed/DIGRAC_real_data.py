9391962d010a5eaecc167452c3970e193a62f71d
SherylHYX <He_YX@outlook.com>
2022-02-21 11:29:38 +0000

    typo

--- torch_geometric_signed_directed/data/directed/DIGRAC_real_data.py
+++ torch_geometric_signed_directed/data/directed/DIGRAC_real_data.py
@@ -11,7 +11,7 @@ class DIGRAC_real_data(InMemoryDataset):
     `DIGRAC: Digraph Clustering Based on Flow Imbalance" <https://arxiv.org/pdf/2106.05194.pdf>`_ paper.
     
     Args:
-        name (str): Name of the data set, choices are: 'blog', 'wikitalk', 'migration', 'lead_lag"+str(year) (year from 2000 to 2019).
+        name (str): Name of the data set, choices are: 'blog', 'wikitalk', 'migration', 'lead_lag"+str(year) (year from 2001 to 2019).
         root (str): Root directory where the dataset should be saved.
         transform (callable, optional): A function/transform that takes in an :obj:`torch_geometric.data.Data` object and returns a transformed
             version. The data object will be transformed before every access.
