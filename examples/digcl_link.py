fix bugs in link split for sign graphs; update link examples

examples/digcl_link.py
@@ -6,7 +6,7 @@ import torch
 from sklearn import metrics
 
 from torch_geometric_signed_directed.utils import (
-    directed_link_class_split, in_out_degree, cal_fast_appr, drop_feature, pred_digcl_link)
+    link_class_split, in_out_degree, cal_fast_appr, drop_feature, pred_digcl_link)
 from torch_geometric_signed_directed.nn.directed import DiGCL
 from torch_geometric_signed_directed.data import load_directed_real_data
 
@@ -47,7 +47,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 dataset_name = args.dataset.split('/')
 data = load_directed_real_data(dataset=dataset_name[0], root=path, name=dataset_name[1]).to(device)
-link_data = directed_link_class_split(data, prob_val=0.15, prob_test=0.05, task = 'direction', device=device)
+link_data = link_class_split(data, prob_val=0.15, prob_test=0.05, task = 'direction', device=device)
 
 model = DiGCL(in_channels=2, activation='relu',
                  num_hidden=32, num_proj_hidden=16,

examples/digcn_inception_link.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 
 from torch_geometric_signed_directed.utils import (
-    directed_link_class_split, in_out_degree, 
+    link_class_split, in_out_degree, 
     get_second_directed_adj, get_appr_directed_adj)
 from torch_geometric_signed_directed.nn.directed import DiGCN_Inception_Block_link_prediction
 from torch_geometric_signed_directed.data import load_directed_real_data
@@ -43,7 +43,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 dataset_name = args.dataset.split('/')
 data = load_directed_real_data(dataset=dataset_name[0], root=path, name=dataset_name[1]).to(device)
-link_data = directed_link_class_split(data, prob_val=0.15, prob_test=0.05, task = 'direction', device=device)
+link_data = link_class_split(data, prob_val=0.15, prob_test=0.05, task = 'direction', device=device)
 
 model = DiGCN_Inception_Block_link_prediction(num_features=2, hidden=16, label_dim=2, dropout=0.5).to(device)
 criterion = nn.NLLLoss()

torch_geometric_signed_directed/utils/general/link_split.py
@@ -174,18 +174,17 @@ def link_class_split(data:torch_geometric.data.Data, size:int=None, splits:int=1
 
         if task == 'sign':
             nmst = np.array(nmst)
-            exist = np.array(np.abs(A[nmst[:,0], nmst[:,1]] > 0)).flatten()
+            exist = np.array(np.abs(A[nmst[:,0], nmst[:,1]]) > 0).flatten()
             if np.sum(exist) < len(nmst):
                 nmst = nmst[exist]
 
-            ids_test = nmst[:len_test]
-            ids_val = nmst[len_test:len_test+len_val]
-            ids_train = nmst[len_test+len_val:max_samples]
-            ids_train = np.array(ids_train.tolist()+mst)
+            ids_test = nmst[:len_test].copy()
+            ids_val = nmst[len_test:len_test+len_val].copy()
+            ids_train = np.array(nmst[len_test+len_val:max_samples].tolist()+mst)
 
-            labels_test = 1.0*np.array(A[ids_test[:,0], ids_test[:,1]] > 0).flatten()
-            labels_val = 1.0*np.array(A[ids_val[:,0], ids_val[:,1]] > 0).flatten()
-            labels_train = 1.0*np.array(A[ids_train[:,0], ids_train[:,1]] > 0).flatten()
+            labels_test  = 1.0*np.array(A[ids_test[:,0],ids_test[:,1]] > 0).flatten()
+            labels_val   = 1.0*np.array(A[ids_val[:,0],ids_val[:,1]] > 0).flatten()
+            labels_train = 1.0*np.array(A[ids_train[:,0],ids_train[:,1]] > 0).flatten()
             undirected_train = np.array([])
         else:
             ids_test = nmst[:len_test]+neg_edges[:len_test]
@@ -215,7 +214,7 @@ def link_class_split(data:torch_geometric.data.Data, size:int=None, splits:int=1
         
         # set up the observed graph and weights after splitting
         oberved_edges = -np.ones((len(ids_train),2), dtype=np.int32)
-        oberved_weight = -np.ones((len(ids_train),1), dtype=np.float32)
+        oberved_weight = np.zeros((len(ids_train),1), dtype=np.float32)
 
         direct = (np.abs(A[ids_train[:, 0], ids_train[:, 1]].data) > 0).flatten()
         oberved_edges[direct,0] = ids_train[direct,0]
@@ -227,8 +226,9 @@ def link_class_split(data:torch_geometric.data.Data, size:int=None, splits:int=1
         oberved_edges[direct,1] = ids_train[direct,0]
         oberved_weight[direct,0] = np.array(A[ids_train[direct,1], ids_train[direct,0]]).flatten()
 
-        oberved_edges = oberved_edges[np.sum(oberved_edges, axis=-1) >= 0] 
-        oberved_weight = oberved_weight[oberved_weight >= 0][:,None]
+        valid = (np.sum(oberved_edges, axis=-1) >= 0)
+        oberved_edges = oberved_edges[valid] 
+        oberved_weight = oberved_weight[valid]
 
         # add undirected edges back
         if len(undirected_train) > 0: