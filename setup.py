c2dbede0171424cdf7bae7c45c6c1e19a862bcfd
SherylHYX <He_YX@outlook.com>
2022-06-06 20:12:27 +0000

    0.8.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 url = "https://github.com/SherylHYX/pytorch_geometric_signed_directed"
-__version__ = '0.7.2'
+__version__ = '0.8.0'
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
