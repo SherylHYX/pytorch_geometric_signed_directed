baad164c282992e8cc9101cc8e6af0c5ab99d2cb
SherylHYX <He_YX@outlook.com>
2023-08-11 01:29:25 +0000

    python 3.7

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
