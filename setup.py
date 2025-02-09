518306d47dd4e5d55d0ad8dd47f31b56f3de5335
Yixuan He <He_YX@outlook.com>
2025-02-09 06:21:59 +0000

    update py39 for docs

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
